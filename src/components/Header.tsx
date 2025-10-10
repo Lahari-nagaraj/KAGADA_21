60ddb3783794eb7765df8d1dac301abd44ba3fee
Rahitaputhran <puthranrahita@gmail.com>
2025-10-10 22:39:29 +0530

    kagada png

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
-import logo from "../assets/logo.png";
+import logo from "../assets/kagada 2025.png";
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
